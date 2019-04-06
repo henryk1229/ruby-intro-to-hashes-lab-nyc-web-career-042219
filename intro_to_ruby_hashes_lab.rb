f23a72f7f533f93f4d8ed41a6380f63386d6abb9
Henry Koehler <henryk1229@gmail.com>
2019-04-06 18:12:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,9 @@
 def new_hash
-  {}
+  new_hash = {}
 end
 
 def actor
-  {:name => 'Dwayne The Rock Johnson'}
+  actor = {:name => 'Dwayne The Rock Johnson'}
 
 end
 
@@ -13,7 +13,11 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly = {:railroads => {}}
+	monopoly = {:railroads => {
+	  :pieces => 4,
+	  :names => {},
+	  :rent_in_dollars => {},
+	}}
 
 end
 
